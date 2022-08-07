317e584497aa8305333a186c73134d1c4acec4e3
giangdh <giangdh@dts.com.vn>
2022-08-07 15:25:58 +0700

    Update code

--- SQL/Testing_System_Assignment_3.sql
+++ SQL/Testing_System_Assignment_3.sql
@@ -29,7 +29,7 @@ WHERE LENGTH(Fullname) = (SELECT MAX(LENGTH(Fullname)) FROM `CTE_DEP3`)
 ORDER BY Fullname;
 
 -- Câu 6: Lấy ra tên group đã tạo trước ngày 20/12/2019
-SELECT 
+SELECT DepartmentGroup
     GroupName
 FROM
     `Group`
