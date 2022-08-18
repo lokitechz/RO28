265caf82fff0fec19effa94ed872bd8c54b91a48
giangdh <giangdh@dts.com.vn>
2022-08-18 19:38:54 +0700

    Add asm5 & asm6

---
+++ SQL/Testing_System_Assignment_5.sql
@@ -0,0 +1,76 @@
+USE Testing_System_Assignment_1;
+
+-- Câu 1: Tạo view có chứa danh sách nhân viên thuộc phòng ban Sale
+-- Cách 1: Sử dụng view
+CREATE OR REPLACE VIEW V_DSNV_Sale AS
+    SELECT 
+        A.*, D.DepartmentName
+    FROM
+        `Account` A JOIN
+        `Department` D ON A.DepartmentID = D.DepartmentID
+    WHERE
+        D.DepartmentName = 'Sale';
+        
+SELECT * FROM V_DSNV_Sale ORDER BY AccountID;
+
+-- Cách 2: Sử dụng CTE
+WITH DSNV_Sale AS (
+	SELECT 
+        A.*, D.DepartmentName
+    FROM
+        `Account` A JOIN
+        `Department` D ON A.DepartmentID = D.DepartmentID
+    WHERE
+        D.DepartmentName = 'Sale'
+)
+
+SELECT * FROM DSNV_Sale;
+
+-- Câu 2: Tạo view có chứa thông tin các account tham gia vào nhiều group nhất
+CREATE OR REPLACE VIEW v_account AS
+-- Tạo CTE đếm số lượng Account trong bảng GroupAccount
+WITH CTE_GetListCountAccount AS (
+	SELECT COUNT(AccountID) AS SL
+	FROM `GroupAccount` GA 
+    GROUP BY AccountID
+)
+-- Select và đếm trong CTE
+SELECT A.AccountID, A.Username, COUNT(GA.AccountID) AS SL FROM `GroupAccount` GA
+INNER JOIN account A ON GA.AccountID = A.AccountID
+GROUP BY GA.AccountID
+HAVING COUNT(GA.AccountID) = (
+	SELECT MAX(SL) FROM CTE_GetListCountAccount
+);
+
+SELECT * FROM v_account;
+
+-- Câu 3: Tạo view có chứa câu hỏi có những content quá dài (content quá 18 từ được coi là quá dài) 
+-- Xóa nó đi
+CREATE OR REPLACE VIEW v_contenttren18 AS
+SELECT *
+FROM Question
+WHERE LENGTH(Content) > 18;
+
+SELECT * FROM v_contenttren18;
+
+DELETE FROM v_contenttren18;
+
+-- Câu 4: Tạo view có chứa danh sách các phòng ban có nhiều nhân viên nhất
+CREATE OR REPLACE VIEW V_MaxNV AS
+SELECT D.DepartmentName, COUNT(A.DepartmentID) AS SL
+FROM account A
+INNER JOIN `department` D ON D.DepartmentID = A.DepartmentID
+GROUP BY A.DepartmentID
+HAVING COUNT(A.DepartmentID) = (SELECT MAX(countDEP_ID) AS maxDEP_ID FROM
+(SELECT COUNT(A1.DepartmentID) AS countDEP_ID FROM account A1
+GROUP BY A1.DepartmentID) AS TB_countDepID);
+
+SELECT * FROM V_MaxNV;
+
+-- Câu 5: Tạo view có chứa tất các các câu hỏi do user họ Nguyễn tạo
+CREATE OR REPLACE VIEW V_QuestionCreateByNguyen AS
+SELECT Q.QuestionID, Q.Content, A.FullName AS Creator FROM question Q
+INNER JOIN `account` A ON A.AccountID = Q.CreatorID
+WHERE SUBSTRING_INDEX( A.FullName, ' ', 1 ) = 'Nguyễn';
+
+SELECT * FROM V_QuestionCreateByNguyen;
