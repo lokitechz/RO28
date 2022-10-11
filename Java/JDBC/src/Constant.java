ac8292841b74978fbed7eb75b0cbee392cc55fe1
giangdh <giangdh@dts.com.vn>
2022-10-11 20:09:08 +0700

    Demo JDBC

---
+++ Java/JDBC/src/Constant.java
@@ -0,0 +1,12 @@
+public class Constant {
+
+	public static final String databaseUrl = "jdbc:mysql://localhost:3306/Testing_System_Assignment_1" +
+			"?autoReconnect=true" +
+			"&useSSL=false" +
+			"&characterEncoding=UTF-8";
+
+	public static final String userName = "root";
+
+	public static final String password = "talamvua";
+
+}
