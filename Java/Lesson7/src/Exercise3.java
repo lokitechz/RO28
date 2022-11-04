acbc55a44e22e5a001c3af88e97e750ecaa32169
giangdh <giangdh@dts.com.vn>
2022-11-04 21:41:34 +0700

    Update code exercise 3

---
+++ Java/Lesson7/src/Exercise3.java
@@ -0,0 +1,39 @@
+import java.io.File;
+
+public class Exercise3 {
+
+	public static final String Q1 = "/Users/jacksmacbookpro/Desktop/demo.txt";
+
+	public static final String Q2 = "/Users/jacksmacbookpro/Desktop";
+
+	public static void main(String[] args) {
+		System.out.println("Kết quả câu 1");
+		isFileExists(Q2);
+		System.out.println("Kết quả câu 2");
+		getAllFileName(Q2);
+	}
+
+	public static void isFileExists(String pathFile) {
+		File file = new File(pathFile);
+		boolean isFile = file.isFile();
+		if (isFile) {
+			System.out.println("Đường dẫn này là: " + isFile);
+		}
+		boolean result = file.exists();
+		if (result) {
+			System.out.println("File có tồn tại");
+		} else {
+			System.out.println("File không tồn tại");
+		}
+	}
+
+	public static void getAllFileName(String pathFile) {
+		File file = new File(pathFile);
+		if (file.list().length > 0) {
+			for (String fileName : file.list()) {
+				System.out.println(fileName);
+			}
+		}
+	}
+
+}
